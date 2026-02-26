8ab42cec72df8e22b8a252fed2b8ffefa22d626a
giovbars <giovannybarsobia04@gmail.com>
2026-02-26 01:38:45 -0800

    added create table sql

--- schema.sql
+++ schema.sql
@@ -4,6 +4,13 @@ CREATE DATABASE chicken_grazing_monitoring;
 USE chicken_grazing_monitoring;
 
 
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    date_info DATE NOT NULL
+);
+
+
 CREATE TABLE chickens (
     chicken_id INT AUTO_INCREMENT PRIMARY KEY,
     breed VARCHAR(50)
