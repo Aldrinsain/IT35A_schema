ffdde00cdd2fb4d8f02a69d5d59f1ec5142fb783
Aldrinsain <angelasain86@gmail.com>
2026-02-26 01:08:42 -0800

    added user.sql

--- schema.sql
+++ schema.sql
@@ -4,13 +4,7 @@ CREATE DATABASE chicken_grazing_monitoring;
 USE chicken_grazing_monitoring;
 
 
-CREATE TABLE users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    date_info DATE NOT NULL       
-);
-
-
+c
 CREATE TABLE chickens (
     chicken_id INT AUTO_INCREMENT PRIMARY KEY,
     breed VARCHAR(50)
